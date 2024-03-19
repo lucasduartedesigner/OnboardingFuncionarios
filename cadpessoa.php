2af5507e770e9d6bbe0dec698969452de7c3d0d7
rafaelfelippe1 <105463411+rafaelfelippe1@users.noreply.github.com>
2024-03-19 13:07:55 -0300

    Update cadpessoa.php
    Foi Acrescentado um método de conexão dentro do código
    com linha comentadas explicando

--- cadpessoa.php
+++ cadpessoa.php
@@ -1,5 +1,21 @@
 <?php
 session_start(); //Iniciar a sessao
+
+//Criar conexão com o banco nesse bloco de codigo
+$host = "127.0.0.1"; // endereço do Banco de dados
+$user = "root"; //seu usuario de acesso ao banco de dados
+$pass = ""; // sua senha de acesso ao banco de dados
+$dbname = "portalfuncionarios"; // nome do banco de dados
+$port = 3308; // porta onde esta alocado o banco de dados
+
+// Cria uma Conexão
+$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+
+// Verificar conexão
+//if ($conn->connect_error) {
+ //   die("Conexão falhou: " . $conn->connect_error);
+//}
+
 ?>
 <!DOCTYPE HTML>
 <html lang="pt-br">
@@ -36,11 +52,21 @@ session_start(); //Iniciar a sessao
         <input type="text" name="numero" id="numero" placeholder="Numero da Casa/APT"><br><br>
         <label>Cidade: </label>
         <input type="text" name="cidade" id="cidade" placeholder="Cidade"><br><br>
-        <label>Estado: </label>
-        <input type="text" name="estado" id="estado" placeholder="Estado"><br><br>
+        <select name="estados" id="estados">
+            <option>Selecione o Estado</option>
+            <?php
+            $result_estados = $conn->query("SELECT * FROM estados");
+            //$resultado_estados = mysqli_query($conn, $result_estados);
+            while($row_estados = $result_estados->fetch(PDO::FETCH_ASSOC)){ ?>
+                <option value="<?php echo $row_estados['idestados']; ?>"><?php echo $row_estados['nome']; ?>
+                </option> <?php
+            }
+            ?>
 
+        </select><br><br>
+        <input type="submit" value="Cadastrar">
+        
 
-        <input type="submit" value="Cadastrar" name="CadUsuario">
     </form>
 
 </body>
