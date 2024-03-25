ce4799e58818ae2e8893f4c379b31cd12772dd5b
SrGuedes <125597188+SrGuedes@users.noreply.github.com>
2024-03-25 14:53:03 -0300

    Add files via upload
    atualização dos textos e imagens dos campos

---
+++ OnboardingFuncionarios-main/agendamento.php
@@ -0,0 +1,25 @@
+<?php include_once "template/config.php"; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+  <?php include_once "template/head.php"; ?>
+
+  <body>
+
+    <?php include_once "template/header.php"; ?>
+
+    <main role="main" class="container">
+      <div class="row mt-1">
+        <div class="col-md-12">
+          <h2 class="featurette-heading fw-normal lh-1 mb-3 mt-5">Agendamento de Reuniões e Treinamentos</h2>
+          <p class="mb-4 mt-2"> A plataforma permite agendar reuniões individuais com membros da equipe, sessões de treinamento e apresentações sobre diferentes aspectos da empresa, sua cultura e processos de trabalho.</p>
+        </div>
+      </div>
+    </main>
+
+    <?php include_once "template/footer.php"; ?>
+
+    <?php include_once "template/js.php"; ?>
+
+  </body>
+</html>
---
+++ OnboardingFuncionarios-main/php/valida_login.php
@@ -0,0 +1,66 @@
+<?php 
+
+    // Inicia a sessão 
+    session_start(); 
+
+    //Incluir a conexao com BD
+    include_once "../conn/conexao.php";
+
+    include_once "class/menu.php";
+
+    //Receber os dados do formulario
+    $matricula = $_POST['matricula'];
+    $senha     = $_POST['senha'];
+
+    //Monta consulta em uma string
+    $consulta = "SELECT p.token, p.matricula, p.nome, p.email, p.senha, p.id_campus, c.nome campus, c.titulo, c.texto, c.url, c.imagem 
+                 FROM pessoa p
+                 INNER JOIN campus c ON p.id_campus = c.id_campus
+                 WHERE p.matricula = :matricula
+                 AND p.status IS NOT NULL ";
+
+    //Prepara a consulta para o banco
+    $response = $conn->prepare($consulta);
+
+    //Passa os parametros via bind para evitar SQL Inject
+    $response->bindParam(':matricula', $matricula, PDO::PARAM_STR);
+
+    //Executa a consulta 
+    $response->execute();
+
+    //Verifica se existe dados para retornar
+    if ($response->rowCount() > 0) 
+    {
+        //Coloca os dados retornados em uma variavel
+        $resultado = $response->fetch(PDO::FETCH_ASSOC);
+ 
+        if (password_verify($senha, $resultado['senha'])) 
+        {
+            //Inclui os dados retornado em sessão para conseguir manter usuario logado durante uso do navegador 
+            $_SESSION['token']     = $resultado['token'];
+            $_SESSION['matricula'] = $resultado['matricula'];
+            $_SESSION['nome']      = $resultado['nome'];
+            $_SESSION['email']     = $resultado['email'];
+            $_SESSION['id_campus'] = $resultado['id_campus'];
+            $_SESSION['campus']    = $resultado['campus'];
+            $_SESSION['titulo']    = $resultado['titulo'];
+            $_SESSION['texto']     = $resultado['texto'];
+            $_SESSION['url']       = $resultado['url'];
+            $_SESSION['imagem']    = $resultado['imagem'];
+
+            $menuManager = new MenuManager($conn);
+
+            $_SESSION['menus'] = $menuManager->getMenus();
+        }
+        else
+        {
+            echo "Senha incorreta, tente novamente!";
+        }
+    }
+    else
+    {
+        //Mensagem caso não encontre o cadastro no banco
+        echo "Seus dados não estão corretos.<br>Ou ainda não é cadastrado!";
+    }
+
+?>
