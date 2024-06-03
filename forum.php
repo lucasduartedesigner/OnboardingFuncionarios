2c171b193ee77a50ce9abb963aa330ae48b149b9
Hugo <hgmello25@gmail.com>
2024-06-02 22:15:31 -0300

    Correção /list/forum

--- forum.php
+++ forum.php
@@ -1,62 +1,128 @@
-<?php include_once "template/config.php"; ?>
-<!DOCTYPE html>
-<html lang="pt-br">
+<div class="row">
+    <div class="main-body p-0">
+        <div class="inner-wrapper">
+            <!-- Inner sidebar -->
+            <div class="inner-sidebar">
+                <!-- Inner sidebar header -->
+                <div class="inner-sidebar-header justify-content-center">            
+                    <button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#mensagem-modal">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" 
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus mr-2">
+                            <line x1="12" y1="5" x2="12" y2="19"></line>
+                            <line x1="5" y1="12" x2="19" y2="12"></line>
+                        </svg>
+                        Nova mensagem
+                    </button>
+                </div>
+            
+                <!-- Inner sidebar body / menu lateral de filtros -->
+                <div class="inner-sidebar-body p-0">
+                    <div class="p-3 h-100" data-simplebar="">
+                        <div class="simplebar-wrapper" style="margin: -16px;">
+                            <div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div>
+                                <div class="simplebar-mask">
+                                    <div class="simplebar-offset" style="right: 0px; bottom: 0px;">
+                                        <div class="simplebar-content-wrapper" style="height: 100%">
+                                            <div class="simplebar-content"  style="padding: 16px;">
+                                                <nav class="nav nav-pills nav-gap-y-1 flex-column">
+                                                    <?php include 'list/topicos.php'; ?>
+                                                </nav>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <div class="simplebar-placeholder" style="width: 234px; height: 292px;"></div>
+                        </div>                      
+                    </div>
+                </div>
+                <!-- /Inner sidebar body -->
+            </div>
+        
+          <!-- /Inner sidebar -->
 
-  <?php include_once "template/head.php"; ?>
+          <!-- Inner main -->
+        <div class="inner-main ">
+              <!-- Inner main header -->
+              <div class="inner-main-header justify-content-end"> 
+                  <span class="input-icon input-icon-sm ml-auto w-auto">
+                  <form class="d-flex ">                                 
+                      <input type="text" class="form-control bg-gray-200 border-gray-200 shadow-none mb-4 mt-4" placeholder="Digite sua busca"/>
+                      <button type="button" class="btn btn-success mb-4 mt-4 " data-bs-toggle="modal" data-bs-target="#modalTarefa" Small button >Buscar</button>
+                  </form>
+                  </span>
+              </div>
+              <!-- /Inner main header -->
 
-<body>
+              <!-- Inner main body -->
 
-<?php include_once "template/header.php"; ?>
+              <!-- Forum List -->
+              <?php   
+              
+              $condicao = "";
+              
+              if(!empty($_GET['id_topicos'])) {
+              $condicao = "WHERE t.id_topicos = :id_topicos";
+              }
 
-    <main role="main" class="container">
-        <section class="py-1 text-center container">
-            <div class="row py-lg-2">
-                <div class="col-lg-6 col-md-8 mx-auto">
-                <h1 class="fw-light"><b>Fórum</b></h1>
-                    <p class="lead text-body-secondary">Um fórum interativo onde os colaboradores podem fazer perguntas, compartilhar experiências e receber orientações de colegas 
-            mais experientes e da equipe de recursos humanos.</p>
+                    $consulta = "SELECT f.id_forum_perguntas, f.titulo_pergunta, f.id_pessoa, f.id_departamento, 
+                    f.descricao_pergunta, p.nome, f.data_pergunta , f.visualizacao, f.qtd_resposta
+                    FROM forum_perguntas f 
+                    inner join pessoa p on f.id_pessoa = p.id_pessoa
+                    left join forum_topicos t on f.id_topico = t.id_topicos
+                    $condicao";
+
+                    //Prepara a consulta para o banco
+                    $response = $conn->prepare($consulta);
+
+                    if(!empty($_GET['id_topicos'])) {
+                        $response->bindParam(':id_topicos', $_GET['id_topicos'], PDO::PARAM_STR);
+                    }
+                    
+                    //Executa a consulta 
+                    $response->execute();
+
+                    //Verifica se existe dados para retornar
+                    if ($response->rowCount() > 0) {
+                          //Coloca os dados retornados em uma variavel
+                          while ($resultado = $response->fetch(PDO::FETCH_ASSOC)) {                      
+                          ?> 
+                                <div class="inner-main-body1 p-2 collapse forum-content show">
+                                    <div class="card-body ">
+                                        <div class="media forum-item">
+                                                <div class="card text-center">
+                                                    <div class="card-header" style="background: #389C81">
+                                                        <!-- titulo -->
+                                                        <h6> 
+                                                            <a href="forum.php?id=<?= $resultado['id_forum_perguntas'] ?>" class="text-body"> 
+                                                            <?= $resultado['titulo_pergunta'] ?>                                                                                                                                        
+                                                            </a>
+                                                        </h6>                                                            
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <p class="text-secondary"> <?= $resultado['descricao_pergunta'] ?> </p>    
+                                                            <div class="text-muted small ">
+                                                                <!-- icone visualizações -->
+                                                                <span class="d-none d-sm-inline-block"><i class="far fa-eye"></i> <?= $resultado['visualizacao'] ?> Visualizações </span>
+                                                                <!-- icone resposta mesagem -->                                                                    
+                                                                <span><i class="far fa-comment ml-1"> </i> <?= $resultado['qtd_resposta'] ?> Respostas </span>                                                                   
+                                                            </div>  
+                                                    </div>
+                                                    <div class="card-footer ">
+                                                        <p class="text-muted"> Criado por <a href="javascript:void(0)"> 
+                                                        <?= $resultado['nome'] ?></a> há <span class="text-secondary font-weight-bold">
+                                                        <?= datetimeDiferencaEmMinutos($resultado['data_pergunta']) ?> minutos atrás.</span></p>  
+                                                    </div>
+                                                </div>                                    
+                                        </div>                                                                                            
+                                    </div> 
+                                </div>                                     
+                            <?php
+                            }
+                        }
+                    ?>
+                    </div>
                 </div>
             </div>
-        </section>
-    </main>
-
-<main class="container">
-
-<?php 
-    function datetimeDiferencaEmMinutos($dataHora) {
-        // Obter a data e hora atuais
-        $dataAtual = date_create();
-
-        // Definir a data e hora de comparação
-        $dataComparacao = date_create($dataHora);
-
-        // Calcular a diferença
-        $diferenca = date_diff($dataAtual, $dataComparacao);
-
-        // Converter a diferença para minutos
-        $minutosDiferenca = $diferenca->days * 24 * 60 + $diferenca->h * 60 + $diferenca->i;
-
-        return $minutosDiferenca;
-    }
-
-    if(!empty($_GET['id']))
-    {
-        include_once('edit/forum.php');
-    }
-    else
-    {
-        include_once('list/forum.php');
-    }
-
-?>
-</main>
-
-    <?php include_once "modal/mensagem.php"; ?>
-    <?php include_once "modal/resposta.php"; ?>
-    <?php include_once "modal/pergunta.php"; ?>
-    <?php include_once "template/footer.php"; ?>
-
-    <?php include_once "template/js.php"; ?>
-
-</body>
-</html>
+        </div>
+    </div>
+</div>
